047b844a5777568a05d1ab6433c9702712464ed1
esvelpe <esneidervelpe@gmail.com>
2023-03-14 14:55:58 -0500

    Realización de la Tarea4 y el Parcial

---
+++ lectures/Parcial1/CC1152467751/Punto1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "Punto1.h"
+using namespace std;
+
+int main()
+{
+    Factura factura;
+    factura.resumenCompra();
+    factura.obtenerMontoFactura();
+}
